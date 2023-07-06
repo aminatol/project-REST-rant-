ac61399b2a03eadc61b6cb1b532a37c9fa4bf0eb
aminatol <aminatoluwo@gmail.com>
2023-07-05 23:03:50 -0400

    Adding places controller

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,8 @@
-const router = require("express").Router();
+const router = require('express').Router()
 
-router.get("/", (req, res) => {
-  res.send("GET /places");
-});
+router.get('/', (req, res) => {
+    res.send('GET /places')
+})
 
-module.exports = router;
+module.exports = router
+
