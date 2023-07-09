5f6fc8ba15a0c46e799f5341f77a4edeb2cbbd3a
aminatol <aminatoluwo@gmail.com>
2023-07-09 14:18:56 -0400

    Adding image to REST-Rant

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,9 @@ function home() {
     <Def>
       <main>
         <h1>HOME</h1>
+        <div>
+          <img src="/images.platter.jpg" alt="meat platter" />
+        </div>
         <a href="/places">
           <button className="btn-primary">Places Page</button>
         </a>
